c7ef7ec376ea754bf6a6b53198dc8b56b4bdc880
shuchen <shuchen@fangdd.com>
2020-06-11 17:15:46 +0800

    feat: content security policy

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 const express = require('express');
-
+const cookieParser = require('cookie-parser');
 const app = express();
+const comments = {};
 
-// http://localhost:3000/?xss=%3Cscript%3Ealert(1)%3C/script%3E
+app.use(cookieParser());
+
+// 反射型 http://localhost:3000/?xss=<script>alert(1)</script>
 app.get('/', (req, res) => {
-  res.set('X-XSS-Protection', 0);
+  // res.set('X-XSS-Protection', '1;mode=block');
+  console.log(req.cookies);
+  res.cookie('token', 'abcde', { 'httpOnly': true });
+  res.cookie('name', 'shuch', { maxAge: 1000 * 60 * 60 });
+  res.cookie('session', '123', { 'secure': true });
   res.render('index', {
     title: 'xss examle',
     xss: req.query.xss,
   });
 })
 
+app.get('/iframe', (req, res) => {
+  res.render('iframe');
+})
+
+app.get('/setComment', (req, res) => {
+  res.set('X-XSS-Protection', 0);
+  comments.v = req.query.comment;
+  console.log('set comment req received', comments.v);
+  res.send({ type: 2, msg: 'success' });
+});
+
+app.get('/getComment', (req, res) => {
+  // res.json({ type: 2, msg: comments.v });
+  res.send({ type: 2, msg: comments.v });
+});
+
 app.set('view engine', 'ejs');
 
 app.listen(3000, () => {
   console.log('app run on port 3000');
 });
-
-module.exports = app;
